38a72494f273c5a0ce351ac1f38d62d8d125a337
locnht-it <locnht.it@gmail.com>
2024-05-02 08:14:03 +0700

    Create session 01 -  HelloWorld
    Getting started in the world of .C# | .NET SDK

---
+++ Session01-HelloWorld/HappyNewYear/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HappyNewYear; //~~~ package happynewyear; bên Java
+
+class Program
+{
+    static void Main(string[] args) //y chang main() Java và C
+    {
+        Console.WriteLine("Cha`o na(m mo*'i 2024 - Gia'p Thi`n"); //System.out.println(""); Java
+	Console.WriteLine("\nPress any key to exit...");
+	Console.ReadLine(); //chờ ai đó gõ phím thì mới kết thúc app
+    }
+}
